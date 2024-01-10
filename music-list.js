11fbcc89836c29affdceae4da3845d02e77fc452
ImYash14 <93760567+ImYash14@users.noreply.github.com>
2024-01-10 10:24:43 +0530

    Create music-list.js

---
+++ music-list.js
@@ -0,0 +1,47 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+    name: "song 1",
+    artist: "artist 1",
+    img: "music1",
+    src: "music1"
+  },
+    {
+        name: "song 2",
+        artist: "artist 2",
+        img: "music1",
+        src: "music1"
+      },
+     {
+       name: "song 3",
+       artist: "artist 3",
+       img: "music1",
+       src: "music1"
+     },
+     {
+        name: "song 4",
+        artist: "artist 4",
+        img: "music1",
+        src: "music1"
+      },{
+        name: "song 5",
+        artist: "artist 5",
+        img: "music1",
+        src: "music1"
+      },{
+        name: " song 6",
+        artist: "artist 6",
+        img: "music1",
+        src: "music1"
+      },
+  ];
